4b4e587d15d5427479b57da8f43b0e7ff994abae
Prasanth V C <iamprasanthvc@gmail.com>
2023-03-17 14:52:41 +0530

    billing and card detail captions change

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -19,9 +19,7 @@ const Billing = () => (
         invoicing
       </h2>
       <p className={`${styles.paragraph2} max-w-[470px] mt-5`}>
-        Elit enim sed massa etiam. Mauris eu adipiscing ultrices ametodio
-        aenean neque. Fusce ipsum orci rhoncus aliporttitor integer platea
-        placerat.
+        Experience the power of next-gen credit with our Banking System. Faster, smarter, and more rewarding than ever before.
       </p>
 
       <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -6,12 +6,11 @@ const CardDeal = () => (
   <section className={layout.section}>
     <div className={layout.sectionInfo}>
       <h2 className={styles.heading2}>
-        Find a better card deal <br className="sm:block hidden" /> in few easy
+       Explore better Banking Solution <br className="sm:block hidden" /> in few easy
         steps.
       </h2>
       <p className={`${styles.paragraph2} max-w-[470px] mt-5`}>
-        Arcu tortor, purus in mattis at sed integer faucibus. Aliquet quis
-        aliquet eget mauris tortor.ç Aliquet ultrices ac, ametau.
+        It's time to leave traditional credit cards behind. Our cutting-edge technology and personalized rewards make the future of spending a reality.
       </p>
 
       <Button styles={`mt-10`} />
